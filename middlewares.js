4db79e018a2d31401788ebeb11dd6dda77c6e972
Salih Saygi <salih.saygie@gmail.com>
2021-10-12 17:44:40 -0700

    bug fix

--- middlewares.js
+++ middlewares.js
@@ -1,12 +1,12 @@
-const logger = (req, res, next) => {
+export const logger = (req, res, next) => {
   const method = req.method;
   const url = req.url;
-  const time = new Date().getFullYear();
+  const time = new Date();
   console.log(method, url, time);
   next();
 };
 
-const serverError = (err, req, res, next) => {
+export const serverError = (err, req, res, next) => {
   if (err.status) {
     console.error(err.stack);
   }
@@ -15,22 +15,15 @@ const serverError = (err, req, res, next) => {
     .json({ message: err.message || 'Internal Server Error' });
 };
 
-const notFound = (req, res, next) => {
+export const notFound = (req, res, next) => {
   const error = new Error(`Not Found - ${req.originalUrl}`);
   res.status(404);
   next(error);
 };
 
-const errorHandler = (err, req, res) => {
+export const errorHandler = (err, req, res) => {
   res.json({
     message: err.message,
     stack: process.env.NODE_ENV === 'production' ? null : err.stack,
   });
-};
-
-module.exports = {
-  logger,
-  serverError,
-  notFound,
-  errorHandler,
 };
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/', function () {
+app.get('/', function (req, res) {
   res.send('hello there');
 });
 
